d56cee66aacf20485e9c89b9bfa5b6957f89fecc
Abby Chung <67236934+abby-chung@users.noreply.github.com>
2025-08-07 03:43:44 +0000

    update design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link, useParams } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 const posts = [
   {
