c6dabb5e3d13c114f83363368b5785946f8dfc03
Abby Chung <67236934+abby-chung@users.noreply.github.com>
2025-08-10 15:02:49 +0000

    update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,38 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
+// module.exports = {
+//   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
 //   theme: {
-//     extend: {
-//       colors: {
-//         amber: {
-//           50: '#fffbeb',
-//           100: '#fef3c7',
-//           200: '#fde68a',
-//           300: '#fcd34d',
-//           400: '#fbbf24',
-//           500: '#f59e0b',
-//           600: '#d97706',
-//           700: '#b45309',
-//           800: '#92400e',
-//           900: '#78350f',
-//         },
-//       }
-//     },
+//     extend: {},
 //   },
-//   plugins: [
-//     require('@tailwindcss/typography'),
-//   ],
-// }
+//   plugins: [],
+// };
+
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        amber: {
+          50: '#fffbeb',
+          100: '#fef3c7',
+          200: '#fde68a',
+          300: '#fcd34d',
+          400: '#fbbf24',
+          500: '#f59e0b',
+          600: '#d97706',
+          700: '#b45309',
+          800: '#92400e',
+          900: '#78350f',
+        },
+      }
+    },
+  },
+  plugins: [
+    typography,
+  ],
+};
